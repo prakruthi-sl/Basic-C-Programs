ddd978548a3662ac74dd14b508e4d0bd56d2af57
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-02-26 20:33:11 +0530

    Update relational_operators.c

--- Basic-C-Programs/02_Operators/relational_operators.c
+++ Basic-C-Programs/02_Operators/relational_operators.c
@@ -1,5 +1,7 @@
+/* Problem Statement:
+   Write a C program to demonstrate relational operators in C.
+*/
 #include <stdio.h>
-
 int main() {
     int a = 10, b = 20;
     printf("%d > %d = %d\n", a, b, a > b);
