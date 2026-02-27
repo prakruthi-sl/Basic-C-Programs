8980727358f831362aacc01a7844e47aaafb56c2
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-02-27 19:36:55 +0530

    Update largest_of_three.c

--- Basic-C-Programs/03_Conditional_Statements/largest_of_three.c
+++ Basic-C-Programs/03_Conditional_Statements/largest_of_three.c
@@ -1,9 +1,10 @@
+/* Problem Statement:
+   Write a C program to find the largest of three numbers using conditional statements.
+*/
 #include <stdio.h>
-
 int main() {
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
-
     if(a>b && a>c)
         printf("Largest: %d",a);
     else if(b>c)
