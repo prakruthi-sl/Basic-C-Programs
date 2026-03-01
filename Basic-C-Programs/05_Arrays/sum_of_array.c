1c9f26346713f92fbfb12d2c928e9313bbe812ed
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-01 10:13:28 +0530

    Update sum_of_array.c

--- Basic-C-Programs/05_Arrays/sum_of_array.c
+++ Basic-C-Programs/05_Arrays/sum_of_array.c
@@ -1,8 +1,12 @@
+/* Problem Statement:
+   Write a C program to find the sum of elements of an array.
+*/
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int a[5], sum=0;
-    for(int i=0;i<5;i++){
+    for(int i=0;i<5;i++) 
+    {
         scanf("%d",&a[i]);
         sum += a[i];
     }
