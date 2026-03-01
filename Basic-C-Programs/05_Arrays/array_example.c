e32333e4e0385300f90ecafe02a9b3108df066f7
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-01 10:14:20 +0530

    Update array_example.c

--- Basic-C-Programs/05_Arrays/array_example.c
+++ Basic-C-Programs/05_Arrays/array_example.c
@@ -2,7 +2,8 @@
    Write a C program to declare and display elements of an array.
 */
 #include <stdio.h>
-int main() {
+int main() 
+{
     int a[5]={1,2,3,4,5};
     for(int i=0;i<5;i++)
         printf("%d ",a[i]);
