d98bda64fc634b86b9584d619fd2341813cfb25a
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-02 18:20:15 +0530

    Update input_output.c

--- Basic-C-Programs/01_Input_Output/input_output.c
+++ Basic-C-Programs/01_Input_Output/input_output.c
@@ -2,7 +2,8 @@
    Write a C program to take input from the user and display the entered values.
 */
 #include <stdio.h>
-int main() {
+int main() 
+{
     int a;
     printf("Enter a number: ");
     scanf("%d", &a);
