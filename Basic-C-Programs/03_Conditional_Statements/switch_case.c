b59902cc5dab884e96ea6ea45bbcbe6e699b92a8
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-03 19:56:43 +0530

    Update switch_case.c

--- Basic-C-Programs/03_Conditional_Statements/switch_case.c
+++ Basic-C-Programs/03_Conditional_Statements/switch_case.c
@@ -2,14 +2,18 @@
    Write a C program to demonstrate the use of switch-case statement.
 */
 #include <stdio.h>
-int main() {
+int main() 
+{
     int choice;
     printf("1.Add 2.Sub\n");
     scanf("%d",&choice);
 
-    switch(choice) {
-        case 1: printf("Addition"); break;
-        case 2: printf("Subtraction"); break;
+    switch(choice) 
+    {
+        case 1: printf("Addition"); 
+                break;
+        case 2: printf("Subtraction"); 
+                break;
         default: printf("Invalid");
     }
 }
