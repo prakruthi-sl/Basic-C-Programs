2339012645b42b4038a8871a55243c5c24c6f6e1
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-04 17:41:20 +0530

    Update do_while_loop.c

--- Basic-C-Programs/04_Loops/do_while_loop.c
+++ Basic-C-Programs/04_Loops/do_while_loop.c
@@ -2,10 +2,13 @@
    Write a C program to demonstrate the use of a do-while loop.
 */
 #include <stdio.h>
-int main() {
+int main() 
+{
     int i=1;
-    do {
+    do 
+    {
         printf("%d ",i);
         i++;
-    } while(i<=5);
+    } 
+       while(i<=5);
 }
