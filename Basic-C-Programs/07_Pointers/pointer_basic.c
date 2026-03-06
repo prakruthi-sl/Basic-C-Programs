5b12f2ef56509e1381d24f7a90c2b13d14c4bcee
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-06 07:20:23 +0530

    Update pointer_basic.c

--- Basic-C-Programs/07_Pointers/pointer_basic.c
+++ Basic-C-Programs/07_Pointers/pointer_basic.c
@@ -1,6 +1,9 @@
+/* Problem Statement:
+   Write a C program to demonstrate basic pointer operations.
+*/
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     int a=10;
     int *p=&a;
     printf("Value=%d Address=%p",*p,p);
