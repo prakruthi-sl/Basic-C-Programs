c8ccf345f61a455d8c5ebaf38fedb54146a255a9
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-06 07:21:36 +0530

    Update swap_using_pointers.c

--- Basic-C-Programs/07_Pointers/swap_using_pointers.c
+++ Basic-C-Programs/07_Pointers/swap_using_pointers.c
@@ -1,10 +1,13 @@
+/* Problem Statement:
+   Write a C program to swap two numbers using pointers.
+*/
 #include <stdio.h>
-
-void swap(int *a,int *b){
+void swap(int *a,int *b)
+{
     int t=*a; *a=*b; *b=t;
 }
-
-int main() {
+int main() 
+{
     int x=5,y=10;
     swap(&x,&y);
     printf("x=%d y=%d",x,y);
