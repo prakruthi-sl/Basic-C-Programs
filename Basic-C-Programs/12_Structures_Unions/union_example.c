46c5b7d3c8d145de987148a9914658eb29c7bd10
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-14 08:32:52 +0530

    Update union_example.c

--- Basic-C-Programs/12_Structures_Unions/union_example.c
+++ Basic-C-Programs/12_Structures_Unions/union_example.c
@@ -1,11 +1,15 @@
+/* Problem Statement:
+   Write a C program to demonstrate the use of unions.
+*/
 #include <stdio.h>
-
-union Data{
+union Data
+{
     int i;
     float f;
 };
 
-int main(){
+int main()
+{
     union Data d;
     d.i=10;
     printf("%d",d.i);
