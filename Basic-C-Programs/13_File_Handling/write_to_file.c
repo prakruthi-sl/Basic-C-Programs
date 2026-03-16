c4463d35cfdb81462a52717e737a2a206c393332
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-16 23:09:08 +0530

    Update write_to_file.c

--- Basic-C-Programs/13_File_Handling/write_to_file.c
+++ Basic-C-Programs/13_File_Handling/write_to_file.c
@@ -1,6 +1,9 @@
+/* Problem Statement:
+   Write a C program to write data to a file.
+*/
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     FILE *f=fopen("test.txt","w");
     fprintf(f,"Hello File");
     fclose(f);
