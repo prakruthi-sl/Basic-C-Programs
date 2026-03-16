acbbed08411487d8a6c0216b2fbd19681934fb68
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-16 23:07:57 +0530

    Update append_file.c

--- Basic-C-Programs/13_File_Handling/append_file.c
+++ Basic-C-Programs/13_File_Handling/append_file.c
@@ -1,6 +1,9 @@
+/* Problem Statement:
+   Write a C program to append data to an existing file.
+*/
 #include <stdio.h>
-
-int main() {
+int main() 
+{
     FILE *f=fopen("test.txt","a");
     fprintf(f,"\nAppended text");
     fclose(f);
