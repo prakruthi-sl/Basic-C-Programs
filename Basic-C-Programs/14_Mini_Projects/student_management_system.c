19c31a38d535f8f4df5ed811d2d751e61d40ccf5
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-17 20:22:02 +0530

    Update student_management_system.c

--- Basic-C-Programs/14_Mini_Projects/student_management_system.c
+++ Basic-C-Programs/14_Mini_Projects/student_management_system.c
@@ -1,11 +1,15 @@
+/* Problem Statement:
+   Write a C program to implement a student management system using structures and file handling.
+*/
 #include <stdio.h>
-
-struct Student{
+struct Student
+{
     int id;
     char name[20];
 };
 
-int main(){
+int main()
+{
     struct Student s;
     printf("Enter ID and Name: ");
     scanf("%d%s",&s.id,s.name);
