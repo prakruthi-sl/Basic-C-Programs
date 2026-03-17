ab49e07f2f654994586e6d5633385e2fbe58579a
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-17 20:21:07 +0530

    Update simple_calculator.c

--- Basic-C-Programs/14_Mini_Projects/simple_calculator.c
+++ Basic-C-Programs/14_Mini_Projects/simple_calculator.c
@@ -1,9 +1,14 @@
+/* Problem Statement:
+   Write a C program to create a simple calculator using switch-case.
+*/
 #include <stdio.h>
-
-int main(){
-    int a,b; char op;
+int main()
+{
+    int a,b; 
+    char op;
     scanf("%d %c %d",&a,&op,&b);
-    switch(op){
+    switch(op)
+    {
         case '+': printf("%d",a+b); break;
         case '-': printf("%d",a-b); break;
         case '*': printf("%d",a*b); break;
