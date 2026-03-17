250ed46cf3b7d0eb9322100832e64ff79b2e65ec
Prakruthi S L <prakruthi.sl03@gmail.com>
2026-03-17 20:22:59 +0530

    Update bank_management_system.c

--- Basic-C-Programs/14_Mini_Projects/bank_management_system.c
+++ Basic-C-Programs/14_Mini_Projects/bank_management_system.c
@@ -1,15 +1,20 @@
+/* Problem Statement:
+   Write a C program to implement a basic bank management system.
+*/
 #include <stdio.h>
-
-int main(){
+int main()
+{
     float balance=1000;
     int choice;
     printf("1.Deposit 2.Withdraw\n");
     scanf("%d",&choice);
-    if(choice==1){
+    if(choice==1)
+    {
         float d; scanf("%f",&d);
         balance+=d;
     }
-    else if(choice==2){
+    else if(choice==2)
+    {
         float w; scanf("%f",&w);
         balance-=w;
     }
